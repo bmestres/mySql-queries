a66b2a1504f4aa303288087d1541388d59a12ed5
Bernat Mestres <ber.mestres@gmail.com>
2026-07-27 21:02:37 +0200

    feat: added seccond set of queries at tienda_queries

--- ShopDB/tienda_queries.sql
+++ ShopDB/tienda_queries.sql
@@ -172,6 +172,197 @@ FROM producto
 JOIN fabricante ON codigo_fabricante = fabricante.codigo
 ORDER BY producto.nombre;
 
+-- --------------------------------------------------------------------
+-- 23. Returns a list with the product code, product name, manufacturer 
+-- code and manufacturer name (manager number), of all  products in 
+-- the database.
+-- --------------------------------------------------------------------
+SELECT 
+producto.codigo AS 'Código Producto',
+producto.nombre AS 'Nombre Producto',
+codigo_fabricante AS 'Código Fabricante',
+fabricante.nombre AS 'Nombre Fabricante'
+FROM producto
+JOIN fabricante ON codigo_fabricante = fabricante.codigo;
+
+-- --------------------------------------------------------------
+-- 24. Returns the name, price and name of the manufacturer, the 
+-- cheapest product.
+-- --------------------------------------------------------------
+SELECT
+p.nombre AS 'Producto',
+p.precio AS 'Precio',
+f.nombre AS 'Fabricante'
+FROM producto p
+JOIN fabricante f ON codigo_fabricante = f.codigo
+ORDER BY precio ASC
+LIMIT 1;
+
+-- --------------------------------------------------------------
+-- 25. Returns the name of the product, the price and the name of 
+-- the manufacturer, of the most expensive product.
+-- --------------------------------------------------------------
+SELECT
+p.nombre AS 'Producto',
+p.precio AS 'Precio',
+f.nombre AS 'Fabricante'
+FROM producto p
+JOIN fabricante f ON codigo_fabricante = f.codigo
+ORDER BY precio DESC
+LIMIT 1;
+
+-- --------------------------------------------------------------
+-- 26. Retorna una llista amb nom i preu de tots els productes 
+-- del fabricant Lenovo.
+-- --------------------------------------------------------------
+SELECT
+p.nombre AS 'Producto',
+p.precio AS 'Precio'
+FROM producto p
+JOIN fabricante f ON codigo_fabricante = f.codigo
+WHERE f.nombre = 'Lenovo';
+
+-- --------------------------------------------------------------
+-- 27. Returns a list with name and price of all products of the 
+-- manufacturer Crucial that have a price greater than 200€
+-- --------------------------------------------------------------
+SELECT 
+nombre AS 'Producto',
+precio AS 'Precio'
+FROM producto
+WHERE precio > 200;
+
+-- ---------------------------------------------------------------
+-- 28. Returns a list with name, price and manufacturer name of all 
+-- products from Asus, Hewlett-Packard and Seagate manufacturers. 
+-- ---------------------------------------------------------------
+SELECT 
+p.nombre AS 'Producto',
+p.precio AS 'Precio',
+f.nombre AS 'Fabricante'
+FROM producto p
+JOIN fabricante f ON codigo_fabricante = f.codigo
+WHERE 
+f.nombre = 'Asus' OR 
+f.nombre = 'Hewlett-Packard' OR 
+f.nombre = 'Seagate';
+-- ---------------------------------------------------------------
+-- 29. Returns a list with name, price and manufacturer name of all 
+-- products from Asus, Hewlett-Packard and Seagate manufacturers, 
+-- using the IN operator.
+-- ---------------------------------------------------------------
+SELECT 
+p.nombre AS 'Producto',
+p.precio AS 'Precio',
+f.nombre AS 'Fabricante'
+FROM producto p
+JOIN fabricante f ON codigo_fabricante = f.codigo
+WHERE 
+f.nombre IN ('Asus', 'Hewlett-Packard', 'Seagate');
+
+-- -------------------------------------------------------------------
+-- 30. Returns a list with the name, price and manufacturer name 
+-- of the products, where the manufacturer name ends in the letter 'e'.
+-- -------------------------------------------------------------------
+SELECT
+p.nombre AS 'Producto',
+p.precio AS 'Precio',
+f.nombre AS 'Fabricante'
+FROM producto p
+JOIN fabricante f ON codigo_fabricante = f.codigo
+WHERE f.nombre LIKE '%e'; 
+
+-- ---------------------------------------------------------------------
+-- 31. Returns a list with the product name, price and manufacturer name, 
+-- for all those products whose manufacturers contain the letter 'w' in 
+-- their name.
+-- ---------------------------------------------------------------------
+SELECT
+p.nombre AS 'Producto',
+p.precio AS 'Precio',
+f.nombre
+FROM producto p
+JOIN fabricante f ON codigo_fabricante = f.codigo
+WHERE f.nombre LIKE '%w%';
+
+-- -------------------------------------------------------------------------------
+-- 32. Returns a list with the name of the product, price and the name of 
+-- the manufacturer, for all products with a price equal to or greater than a 180. 
+-- Reesults sorted, first by the price in descending order and then by the product 
+-- name in ascending order.
+-- --------------------------------------------------------------------------------
+SELECT
+p.nombre,
+p.precio
+FROM producto p
+JOIN fabricante f ON codigo_fabricante = f.codigo
+WHERE precio >= 180;
+
+-- ---------------------------------------------------------------------
+-- 33. Returns a list with the code and name of manufacturer, only those 
+-- manufacturers who have associated products in the database.
+-- ---------------------------------------------------------------------
+SELECT DISTINCT
+f.codigo AS 'Codigo',
+f.nombre AS 'Fabricante'
+FROM producto
+JOIN fabricante f ON codigo_fabricante = f.codigo;
+
+-- ---------------------------------------------------------------------
+-- 34. Returns a list of all the manufacturers that exist in the database, 
+-- along with the products that each of them has. It also includes the
+-- manufacturers that don't have any product in the database.
+-- ---------------------------------------------------------------------
+SELECT
+f.nombre AS 'Fabricante',
+p.nombre AS 'Producto'
+FROM fabricante f
+LEFT JOIN producto p ON f.codigo = p.codigo_fabricante;
+
+-- --------------------------------------------------------
+-- 35. Returns a list where only the names of manufacturers 
+-- that do not have any associated products appear.
+-- --------------------------------------------------------
+SELECT
+f.nombre AS 'Fabricante'
+FROM fabricante f
+LEFT JOIN producto p ON f.codigo = p.codigo_fabricante
+WHERE p.nombre IS NULL;
+
+-- --------------------------------------------------------
+-- 36. Returns all products from the manufacturer Lenovo. 
+-- (No inner JOIN).
+-- --------------------------------------------------------
+SELECT *
+FROM producto
+WHERE codigo_fabricante = (
+SELECT codigo
+FROM fabricante
+WHERE nombre = 'Lenovo'
+);
+
+-- --------------------------------------------------------------------------------
+-- 37. Returns all data of products that have the same price as the most expensive 
+-- product of the manufacturer Lenovo. (No inner join).
+-- --------------------------------------------------------------------------------
+
+SELECT *
+FROM producto
+WHERE precio = (
+SELECT MAX(precio)
+FROM producto
+WHERE codigo_fabricante = (
+SELECT codigo
+FROM fabricante
+WHERE nombre = 'Lenovo'
+)
+);
+
+
+
+
+
+
 
 
 
