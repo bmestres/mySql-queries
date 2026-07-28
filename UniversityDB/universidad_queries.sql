9f36f38e07bc132d55f99dbd3d040f1118024847
Bernat Mestres <ber.mestres@gmail.com>
2026-07-28 12:56:25 +0200

    feat: updated universidad_queries

--- UniversityDB/universidad_queries.sql
+++ UniversityDB/universidad_queries.sql
@@ -51,3 +51,69 @@ nombre AS 'Asignatura'
 FROM asignatura
 WHERE cuatrimestre = 1;
 
+-- -----------------------------------------------------------------------
+-- 6. Returns a list of teachers together with the name of the department 
+-- to which they are linked.
+-- -----------------------------------------------------------------------
+SELECT
+p.apellido1 AS 'Apellido 1',
+p.apellido2 AS 'Apellido 2',
+p.nombre AS 'Nombre',
+d.nombre AS 'Departamento'
+FROM profesor
+JOIN departamento d ON id_departamento = d.id
+JOIN persona p ON id_profesor = p.id
+WHERE p.tipo = 'profesor';
+
+-- -----------------------------------------------------------------------
+-- 7. Returns a list with the name of the subjects, start year and end year 
+-- of the student's school year with NIF 26902806M.
+-- -----------------------------------------------------------------------
+SELECT 
+asignatura.nombre AS 'Asignatura',
+curso_escolar.anyo_inicio AS 'Año inicio',
+curso_escolar.anyo_fin AS 'Año fin'
+FROM persona
+JOIN alumno_se_matricula_asignatura ON id = id_alumno
+JOIN asignatura ON alumno_se_matricula_asignatura.id_asignatura = asignatura.id
+JOIN curso_escolar ON alumno_se_matricula_asignatura.id_curso_escolar = curso_escolar.id
+WHERE persona.nif = '26902806M';
+
+-- -------------------------------------------------------------------------
+-- 8. Returns a list with the name of all the departments that have teachers 
+-- who teach a subject in the Degree in Computer Engineering
+-- -------------------------------------------------------------------------
+SELECT DISTINCT
+departamento.nombre AS 'Departamento'
+FROM departamento
+JOIN profesor ON departamento.id = profesor.id_departamento
+JOIN asignatura ON profesor.id_profesor = asignatura.id_profesor
+JOIN grado ON asignatura.id_grado = grado.id
+WHERE grado.nombre = 'Grado en Ingeniería Informática (Plan 2015)';
+
+-- -----------------------------------------------------------------------
+-- 9. Returns a list with all the students who have enrolled in a subject 
+-- during the 2018/2019 school year.
+-- -----------------------------------------------------------------------
+SELECT DISTINCT
+persona.nombre AS 'Nombre',
+persona.apellido1 AS 'Apellido 1',
+persona.apellido2 AS 'Apellido 2'
+FROM persona
+JOIN alumno_se_matricula_asignatura ON persona.id = id_alumno
+JOIN curso_escolar ON id_curso_escolar = curso_escolar.id
+WHERE
+curso_escolar.anyo_inicio = '2018' AND
+curso_escolar.anyo_fin = '2019'; 
+
+
+
+
+
+
+
+
+
+
+
+
